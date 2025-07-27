8b3efe857cb53d3f090c6f15e5467d2084400e00
Nikola23-python <89059736980@mail.ru>
2025-07-28 01:45:26 +0700

    Третий вариант (27.07.25)

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,23 +1,56 @@
 from datetime import datetime
 
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 from .models import Post
-
-
+from .filters import PostFilter
+from .forms import PostForm
 class PostsList(ListView):
     model = Post
     ordering = '-created_at'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 10
 
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+"""
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         context['info'] = None
         return context
-
+"""
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
 
